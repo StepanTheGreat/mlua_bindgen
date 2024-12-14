08417713dd2fcf2947e748955808e01e0d702adc
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2024-12-14 18:03:29 +0100

    `register` trait method

--- src/lib.rs
+++ src/lib.rs
@@ -12,5 +12,9 @@ pub use macros::*;
 /// Functions assigned to these tables are constructors. This makes it quite convenient to
 /// both document and also construct types.
 pub trait UserDataTable {
+    /// Get Self as a table. This is automaticaally used inside register when registering a type
     fn as_table(lua: &mlua::Lua) -> mlua::Result<mlua::Table>;
+
+    /// Register this type to a specified table (could be for example the `lua.globals()` table)
+    fn register(lua: &mlua::Lua, to: &mlua::Table) -> mlua::Result<()>;
 }
