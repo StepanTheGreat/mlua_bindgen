dd0e2846a69e209f8202c887d8b4286213672bf5
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2024-12-17 16:02:29 +0100

    Added basic discriminant support for enums

--- macros/src/enums.rs
+++ macros/src/enums.rs
@@ -1,6 +1,6 @@
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, ToTokens};
-use syn::ItemEnum;
+use syn::{Expr, ItemEnum, Lit};
 
 use crate::utils::macro_error;
 
@@ -19,11 +19,35 @@ pub fn expand_enum(input: TokenStream2, item: ItemEnum) -> TokenStream2 {
     let mut value = 0;
     for variant in item.variants.iter() {
         let vname = variant.ident.to_token_stream();
-        if variant.discriminant.is_some() {
-            return macro_error(
-                variant, 
-                "mlua_bindgen enums don't support discriminants currently"
-            )
+        if let Some((_, ref expr)) = variant.discriminant {
+            
+            // Things like negative enums for now don't exist, because apparently negative values aren't 
+            // considered to be litterals???
+            //
+            // In any case, I'm over-checking errors here, since I already got cases where a rust compiler doesn't
+            // complain on negative discriminants, while the lua enum simply skipped the variant.  
+
+            let lit = if let Expr::Lit(lit) = expr { lit } else { 
+                return macro_error(
+                    expr, 
+                    "Failed to parse enum disciminant. Make sure to use positive integer values"
+                );
+            };
+            let lit_int = if let Lit::Int(ref lit_int) = lit.lit { lit_int } else { 
+                return macro_error(
+                    expr, 
+                    "Only integers are accepted in enum discriminants"
+                );
+            };
+
+            if let Ok(val) = lit_int.base10_parse::<usize>() {
+                value = val;
+            } else {
+                return macro_error(
+                    expr, 
+                    "Failed to parse the discriminant. Expected a positive integer value"
+                );
+            }
         }
         variants.push(quote! {
             table.set(stringify!(#vname), #value)?;
